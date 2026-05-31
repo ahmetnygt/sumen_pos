4f8f7f0549efe049886ed502d04818853e8a819c
unknown <ahmetnygt@hotmail.com>
2026-05-31 20:16:02 +0300

    garson ekranina hizli satis eklendi

--- mobile-pwa/src/pages/WaiterOrder.jsx
+++ mobile-pwa/src/pages/WaiterOrder.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import api from '../services/api';
-import './Order.css';
+import './Order.css'; 
 
 const WaiterOrder = () => {
     const { tableId } = useParams();
